390f50be2ba89f044e08a2c8c957414ae9e6e420
Nick Jackson <nickjackson@s172-20-31-h144.paws.uga.edu>
2022-02-19 21:56:13 -0500

    Made bunch of changes

--- src/components/FileList.js
+++ src/components/FileList.js
@@ -1,11 +1,27 @@
-function FileList({ files }) {
+function FileList({ files, removeInstrument }) {
+  function removeTrack(i) {
+    removeInstrument((prevFiles) =>
+      prevFiles.filter((val, index) => {
+        return index !== i;
+      })
+    );
+    console.log(files[i]);
+    console.log(files);
+    // delete e.targ
+  }
   return (
     <div id="file-list">
       {files.map((track, i) => {
+        console.log(files);
         return (
-          <div id="instrument-track" key={i}>
+          <div
+            className="instrument-track"
+            id={`instrument-track-${i}`}
+            key={i}
+            onClick={removeTrack.bind(this, i)}
+          >
             <span>{track.fileName}</span>
-            <audio controls>
+            <audio controls loop id={"soundbar"}>
               <source src={track.instrument} />
             </audio>
             <br></br>
