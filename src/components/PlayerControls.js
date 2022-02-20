44b57de75c517e1e53fa584d0d79fde690e59a5a
Brenton Jackson <bjavonjackson@gmail.com>
2022-02-20 07:25:10 -0500

    add icons

--- src/components/PlayerControls.js
+++ src/components/PlayerControls.js
@@ -11,15 +11,18 @@ function PlayerControls({ isLoaded, isPaused, setIsPaused, selectedTrack }) {
   return (
     <div id="player-control-wrapper">
       <div id="player-controls">
-        <button id="rewind-button">Rewind</button>
-        <button id="play-button" onClick={selectedTrack != null ? play : null}>
-          {document.querySelector(`#soundbar-${selectedTrack}`)
-            ? document.querySelector(`#soundbar-${selectedTrack}`).paused
-              ? "Play"
-              : "Pause"
-            : "Play"}
-        </button>
-        <button id="fast-forward-button">Fast Forward</button>
+        <span id="rewind-button"></span>
+        <span
+          id={
+            document.querySelector(`#soundbar-${selectedTrack}`)
+              ? document.querySelector(`#soundbar-${selectedTrack}`).paused
+                ? "play-button"
+                : "pause-button"
+              : "play-button"
+          }
+          onClick={selectedTrack != null ? play : null}
+        ></span>
+        <span id="fast-forward-button"></span>
       </div>
     </div>
   );
